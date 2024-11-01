08f4e64b1c91519998d97a715c6deb5a6ed1ef5d
Cristian0-Pereira <cristianops2020@gmail.com>
2024-11-01 02:23:27 -0300

    ad

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,7 +1,16 @@
-using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Infrastructure.Db;
 using MinimalApi.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello Cristiano!!!");
@@ -14,3 +23,24 @@ app.MapPost("/login", (LoginDTO loginDTO) => {
 });
 
 app.Run();
+
+// GPT exemplo
+
+// app.MapPost("/login", async (LoginDTO loginDTO, DbContexto db) => {
+//     var administrador = await db.Administradores
+//         .FirstOrDefaultAsync(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha);
+    
+//     if (administrador != null) 
+//         return Results.Ok("Login com sucesso!");
+//     else
+//         return Results.Unauthorized();
+// });
+
+// C:\xampp\mysql\bin\mysql -u root -p
+
+// Validando admin com login 
+
+// Abrir o xampp - startar o apache e o mysql
+// Abrir o Post
+// Abrir o Docker
+// Verificar as conexões antes de iniciar os estudos
--- minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
+++ minimal-api/obj/Debug/net8.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65fa2143defeb4db7c34bffcc21cfc2e6f49df33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f11d9a68592435f2f2681d735170f0a0fa6a7b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
